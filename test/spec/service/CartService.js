93faee13d8e519ef5a25b21729838f982d1f04c6
SnakeWorker <snakerworker@gmail.com>
2014-08-28 17:47:44 +0800

    finish CartService test

--- test/spec/service/CartService.js
+++ test/spec/service/CartService.js
@@ -2,29 +2,63 @@
 
 describe('Service: CartService', function () {
 
-  var localStorageService,service;
+  var localStorageService,service,cart;
   beforeEach(function(){
       module('angularLetusgoApp');
       inject(function ($injector) {
           localStorageService = $injector.get('localStorageService');
           service = $injector.get('CartService');
-      })
-  });
-
-  it('should result.length equal 0', function () {
-      var cart = {
+      });
+      cart = {
           cartItems: [
               {
-                  product: {
-                     // name:
-                  }
+                  product: {name : 'Instant_noodles', unit : 'bag', category : 'grocery', price : 1},
+                  count : 4
               },
-              {},
-              {}],
-          len : 0
+              {
+                  product: {name : 'coca_cola', unit : 'bottle', category : 'grocery', price : 0.5},
+                  count : 3
+              },
+              {
+                  product: {name : 'kettle', unit : 'piece', category : 'device', price : 43.5},
+                  count : 1
+              }
+          ],
+          len : 8
       }
-      spyOn(localStorageService,'get').andReturn({cartItems:[],len:0});
-      expect(1+2).toEqual(3);
+
   });
 
+  it('should get() work', function () {
+      spyOn(localStorageService,'get').andReturn(cart);
+      var result = service.get();
+      expect(result.cartItems[0].product.name).toEqual('Instant_noodles');
+      expect(result.cartItems.length).toBe(3);
+      expect(result.len).toBe(8);
+  });
+
+  it('should set() work',function(){
+      spyOn(localStorageService,'add');
+      service.add({});
+      expect(localStorageService.add).toHaveBeenCalledWith('cart',{});
+  });
+
+  it('should the total money equal 49',function(){
+       var result = service.getTotalMoney(cart);
+      expect(result).toEqual(49);
+  });
+    it('should the total count equal 7',function(){
+        var result = service.getTotalCount(cart);
+        expect(result).toEqual(8);
+    });
+
+    it('should the subtotal equal 43.50',function(){
+        var result = service.getSubtotal(cart.cartItems[2]);
+        expect(result).toBe(43.5.toFixed(2));
+    });
+    it('should remove() work',function(){
+        spyOn(localStorageService,'remove');
+        service.remove();
+        expect(localStorageService.remove).toHaveBeenCalledWith('cart');
+    });
 });
