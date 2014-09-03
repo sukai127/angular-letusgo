2ba8a3cdcaa374bd6bffb1dbcecbe96129b1008c
SnakeWorker <snakerworker@gmail.com>
2014-09-03 12:45:41 +0800

    modify and beautify updateProduct.html

---
+++ app/scripts/controllers/updateProduct.js
@@ -0,0 +1,18 @@
+'use strict';
+
+angular.module('angularLetusgoApp')
+  .controller('UpdateProductCtrl', function ($scope,CategoryManageService,ProductManageService,$routeParams) {
+
+    $scope.products = ProductManageService.loadAllProducts();
+    $scope.product = ProductManageService.getProductByName($routeParams.name);
+    $scope.categories = CategoryManageService.loadAllCategories();
+
+    $scope.getCategoryName = function(id){
+      return CategoryManageService.getCategoryNameById(id);
+    };
+    $scope.updateProduct = function(){
+      ProductManageService.updateProduct($scope.product);
+    };
+
+    $scope.$emit('parent_highLight_active','product');
+  });
