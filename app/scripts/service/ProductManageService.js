d24590e382adcb5c6576f082f4b43fbb1f31a32f
SnakeWorker <snakerworker@gmail.com>
2014-09-09 11:42:16 +0800

    modify ProductManageService.js

--- app/scripts/service/ProductManageService.js
+++ app/scripts/service/ProductManageService.js
@@ -3,11 +3,11 @@ angular.module('angularLetusgoApp')
   .service('ProductManageService', function (localStorageService) {
     this.loadAllProducts = function () {
       var products = [
-        {name: 'Instant_noodles', unit: 'bag', category: '1', price: 1},
-        {name: 'apple', unit: 'kg', category: '1', price: 2.5},
-        {name: 'coca_cola', unit: 'bottle', category: '1', price: 0.5},
-        {name: 'kettle', unit: 'piece', category: '2', price: 43.5},
-        {name: 'fan', unit: 'piece', category: '2', price: 30}
+        {id:1, name: 'Instant_noodles', unit: 'bag', category: '1', price: 1},
+        {id:2, name: 'apple', unit: 'kg', category: '1', price: 2.5},
+        {id:3, name: 'coca_cola', unit: 'bottle', category: '1', price: 0.5},
+        {id:4, name: 'kettle', unit: 'piece', category: '2', price: 43.5},
+        {id:5, name: 'fan', unit: 'piece', category: '2', price: 30}
       ];
       var temp = localStorageService.get('products');
       return temp ? temp : (localStorageService.add('products', products), products);
@@ -15,6 +15,19 @@ angular.module('angularLetusgoApp')
     this.add = function (products) {
       localStorageService.add('products', products);
     };
+
+    this.insert = function(product){
+      var products = this.loadAllProducts();
+      var isExist = _.some(products,{name : name});
+      var isOk = product.name && product.price && product.unit && product && !isExist;
+      if(isOk){
+        var id = parseInt(products[products.length-1].id) + 1;
+        product.id = id;
+        products.push(product);
+      }
+      return products;
+    };
+
     this.updateProduct = function (product) {
       var products = localStorageService.get('products');
       _.forEach(products,function(item,index){
